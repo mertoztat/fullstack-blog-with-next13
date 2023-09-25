7cc416a6f2ce97228e9e192081061d3f7e1678f0
mertoztat <mertoztat@gmail.com>
2023-09-26 00:30:37 +0300

    feat: fetching topics with server side

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -2,19 +2,37 @@ import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
 import RemoveBtn from "./RemoveButton";
 
-export const TopicsList = () => {
+async function getData() {
+  const res = await fetch("http://localhost:3000/api/topics");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export const TopicsList = async () => {
+  const data = await getData();
+
   return (
-    <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
-      <div>
-        <h2 className="font-bold text-2xl">Title</h2>
-        <div>Description</div>
-      </div>
-      <div className="flex gap-2">
-        <RemoveBtn />
-        <Link href={"/editTopic"}>
-          <HiPencilAlt size={24} />
-        </Link>
-      </div>
+    <div className=" flex-col p-4 border border-slate-300 my-3 flex justify-between gap-5">
+      {data?.topics?.map((item) => (
+        <>
+          <div className=" w-full flex justify-between">
+            <div className="mt-2  w-full">
+              <h2 className="font-bold text-2xl">{item.title}</h2>
+              <div>{item.description}</div>
+            </div>
+            <div className="flex gap-2 justify-center items-center">
+              <RemoveBtn />
+              <Link href={"/editTopic"}>
+                <HiPencilAlt size={24} />
+              </Link>
+            </div>
+          </div>
+        </>
+      ))}
     </div>
   );
 };
