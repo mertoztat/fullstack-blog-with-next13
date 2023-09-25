e26a14daec5f2dcbbd800824e50faa2bca94c1e5
mertoztat <mertoztat@gmail.com>
2023-09-26 01:05:52 +0300

    feat: delete post

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -17,23 +17,25 @@ async function getData() {
 export const TopicsList = async () => {
   const data = await getData();
 
+  console.log("qwewqe : ", data.topics);
+
   return (
     <div className=" flex-col p-4 border border-slate-300 my-3 flex justify-between gap-5">
       {data?.topics?.map((item) => (
-        <>
+        <div key={item._id}>
           <div className=" w-full flex justify-between">
             <div className="mt-2  w-full">
               <h2 className="font-bold text-2xl">{item.title}</h2>
               <div>{item.description}</div>
             </div>
             <div className="flex gap-2 justify-center items-center">
-              <RemoveBtn />
+              <RemoveBtn id={item?._id} />
               <Link href={`/editTopic/${item._id}`}>
                 <HiPencilAlt size={24} />
               </Link>
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
